092619cd69d980d46062283ed055971d085caa3a
Gabriel Silva <biel14022002@gmail.com>
2022-09-19 01:01:12 -0300

    Aula2 + bonus

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,4 +1,7 @@
+ import {MagnifyingGlassPlus} from 'phosphor-react'
+
  import './styles/main.css';
+
  import LogoImg from './assets/logonlwesports.svg'
 
 function App() {
@@ -57,9 +60,21 @@ function App() {
           </div>
         </a>
       </div>
+    
 
-      <div className='bg-[#2a2634] px-8 py-6 self-stretch mt-8 rounded-lg '>
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 '>
+        <div className='bg-[#2a2634] px-8 py-6 self-stretch flex justify-between items-center '>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
+          </div>
 
+        <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex itens-center gap-3'>
+          <MagnifyingGlassPlus size={24}/>
+          Publicar anuncio
+        </button>
+
+        </div>
       </div>
 
     </div>
